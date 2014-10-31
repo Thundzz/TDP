fc0dce59145409e13c5253b9ab6c77b198d1df63
Thundzz <yacinenew@gmail.com>
2014-10-31 18:10:17 +0100

    modify the test file

--- 2/main.c
+++ 2/main.c
@@ -1,18 +1,19 @@
 #include "particule.h"
+#include <stdio.h>
 
-
-#define NBITER 10000
+#define NBITER 10
 
 int main()
 {
 	pset *s = pset_alloc(1);
 	pset *s2= pset_alloc(1);
-
 	pset_init_rand(s);
 	pset_init_rand(s2);
 
 	pset_print(s);
 	pset_print(s2);	
+
+	FILE * fichier =fopen("deux.txt", "w+");
 
 	for (int i = 0; i < NBITER ; ++i)
 	{
@@ -20,11 +21,16 @@ int main()
 		f_grav(s2, s);
 		pset_step(s);
 		pset_step(s2);
+		fprintf(fichier, 
+			"%d %g %g %g %g\n",
+			i, s->pos[0], s->pos[1], s2->pos[0], s2->pos[1]);
 	}
 
 	pset_print(s);
 	pset_print(s2);	
 	
+
+	fclose(fichier);
 	pset_free(s);
 	pset_free(s2);
 	return 0;
--- 2/particule.c
+++ 2/particule.c
@@ -7,11 +7,9 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-double dt = 1.0;
+double dt = 1000.0;
 static int SEEDED =0;
 
-
-// http://www.concentric.net/~Ttwang/tech/inthash.htm
 unsigned long mix(unsigned long a, unsigned long b, unsigned long c)
 {
     a=a-b;  a=a-c;  a=a^(c >> 13);
