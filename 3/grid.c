672f47501ca257a27def9ddc25ecf7148286c108
Nico <nheng@enseirb.fr>
2014-11-16 20:57:56 +0100

    passage des dimensions de la grille en parametre

--- 3/grid.c
+++ 3/grid.c
@@ -105,6 +105,27 @@ int main(int argc, char** argv) {
 	MPI_Comm_rank( MPI_COMM_WORLD, &myrank); 
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
 
+	if(argc != 3)
+	{
+		if(myrank == 0){
+			printf("Syntax: ./a.out 'nb_procs_per_row' 'nb_procs_per_col'\n");
+		}
+		MPI_Finalize();
+		return -1;
+	}
+
+	int np_r = atoi(argv[1]); //Number of procs per row
+	int np_c = atoi(argv[2]); //Number of procs per column
+
+	if(np != (np_r * np_c))
+	{
+		if(myrank == 0){
+			printf("Number of processes must be equal to the product of the two args\n");
+		}
+		MPI_Finalize();
+		return -1;
+	}
+
 	//Global matrix
 	matrix a;
 	//double a[N*N];
@@ -130,8 +151,15 @@ int main(int argc, char** argv) {
 	}
 	//End of Global matrix
 
-	int np_r = 1; //Number of procs per row
-	int np_c = 1; //Number of procs per column
+	if(N % np_r != 0 || N % np_c != 0)
+	{
+		if(myrank == 0){
+			printf("Error: Grid dimensions must divide matrix dimensions\n");
+			matrix_free(&a);
+		}
+		MPI_Finalize();
+		return -1;
+	}
 
 	double* b;
 	MPI_Datatype type_block;
