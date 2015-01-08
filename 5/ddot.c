2c73011dd33f257c4c0ed3015e8be7787e24b815
Thundzz <yacinenew@gmail.com>
2015-01-08 14:11:37 +0100

    Fix a problem with ddot

--- 5/ddot.c
+++ 5/ddot.c
@@ -5,8 +5,8 @@ double cblas_ddot(const int N, const double *X, const int incX,
                   const double *Y, const int incY)
 {
   int i;
-  double res= X[0] + Y[0];
-  for(i = 1; i < N; i ++)
+  double res= 0;
+  for(i = 0; i < N; i ++)
   {
     res += X[i * incX] * Y[i* incY];
   }
