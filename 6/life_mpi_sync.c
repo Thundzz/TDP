ae8e78c1468f5dd232d62f3af0bb67578bb6a41d
Thundzz <yacinenew@gmail.com>
2015-01-29 17:13:26 +0100

    Clean the code

--- 6/life_mpi_sync.c
+++ 6/life_mpi_sync.c
@@ -7,7 +7,7 @@
 //#define PRINT_ALIVE
 // #define OUTPUT_BOARD
 
-#define BS 9
+#define BS 10
 
 #define cell( _i_, _j_ ) board[ ldboard * (_j_) + (_i_) ]
 #define ngb( _i_, _j_ )  nbngb[ ldnbngb * ((_j_) - 1) + ((_i_) - 1 ) ]
@@ -61,15 +61,13 @@ int main(int argc, char* argv[])
 {	
 	MPI_Init(NULL, NULL);
 	int myrank, nb_processes;
-	MPI_Request req;
-  	MPI_Status st;
   	MPI_Comm_rank( MPI_COMM_WORLD, &myrank ); 
 	MPI_Comm_size( MPI_COMM_WORLD, &nb_processes);
     
     int i, j, loop, num_alive, maxloop;
     int ldboard, ldnbngb, ldglobboard;
     double t1, t2;
-    double temps;
+    double temps, real_time;
 	int *globboard; 
     int *board;
     int *nbngb;
@@ -99,6 +97,7 @@ int main(int argc, char* argv[])
     if(myrank == 0){
     	num_alive = generate_initial_board( BS, globboard , ldglobboard );
 	    output_board( BS, globboard+1 + ldglobboard, ldglobboard, 0 );
+    	fprintf(stderr, "Starting number of living cells = %d\n", num_alive);
 	}
 
 	MPI_Datatype block2, block;
@@ -119,11 +118,11 @@ int main(int argc, char* argv[])
 		{
 			counts[i+j*PROCSPERCOL]= 1;
 			displs[i+j*PROCSPERCOL]= i*ldglobboard*(ldboard-2)+j*(ldboard-2);
-			// i = 0,j =1
 		}
 	}
 	MPI_Scatterv(globboard, counts, displs, block, board, ldboard*ldboard,
 				MPI_INT,0, MPI_COMM_WORLD);
+	
 	for (int i = 0; i < nb_processes; ++i)
 	{
 		if(myrank == i){
@@ -131,25 +130,24 @@ int main(int argc, char* argv[])
 		}
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
-    //fprintf(stderr, "Starting number of living cells = %d\n", num_alive);
+
     t1 = mytimer();
-    /*
-    for (loop = 1; loop <= 1; loop++) {
+    int block_size = ldboard-2;
+    for (loop = 1; loop <= maxloop; loop++) {
+	cell(   0, 0   ) = cell(block_size, block_size);
+	cell(   0, block_size+1) = cell(block_size,  1);
+	cell(block_size+1, 0   ) = cell( 1, block_size);
+	cell(block_size+1, block_size+1) = cell( 1,  1);
 
-	cell(   0, 0   ) = cell(BS, BS);
-	cell(   0, BS+1) = cell(BS,  1);
-	cell(BS+1, 0   ) = cell( 1, BS);
-	cell(BS+1, BS+1) = cell( 1,  1);
-
-	for (i = 1; i <= BS; i++) {
-	    cell(   i,    0) = cell( i, BS);
-	    cell(   i, BS+1) = cell( i,  1);
-	    cell(   0,    i) = cell(BS,  i);
-	    cell(BS+1,    i) = cell( 1,  i);
+	for (i = 1; i <= block_size; i++) {
+	    cell(   i,    0) = cell( i, block_size);
+	    cell(   i, block_size+1) = cell( i,  1);
+	    cell(   0,    i) = cell(block_size,  i);
+	    cell(block_size+1,    i) = cell( 1,  i);
 	}
 
-	for (j = 1; j <= BS; j++) {
-		for (i = 1; i <= BS; i++) {
+	for (j = 1; j <= block_size; j++) {
+		for (i = 1; i <= block_size; i++) {
 		ngb( i, j ) =
 		    cell( i-1, j-1 ) + cell( i, j-1 ) + cell( i+1, j-1 ) +
 		    cell( i-1, j   ) +                  cell( i+1, j   ) +
@@ -158,8 +156,8 @@ int main(int argc, char* argv[])
 	}
 
 	num_alive = 0;
-	for (j = 1; j <= BS; j++) {
-		for (i = 1; i <= BS; i++) {
+	for (j = 1; j <= block_size; j++) {
+		for (i = 1; i <= block_size; i++) {
 			if ( (ngb( i, j ) < 2) || 
 			     (ngb( i, j ) > 3) ) {
 			    cell(i, j) = 0;
@@ -174,9 +172,7 @@ int main(int argc, char* argv[])
 	    }
 	}
 
-	#ifdef OUTPUT_BOARD
-    output_board( BS, &(cell(1, 1)), ldboard, 0 );
-	#endif
+
 #ifdef PRINT_ALIVE
 	printf("%d \n", num_alive);
 #endif
@@ -185,12 +181,13 @@ int main(int argc, char* argv[])
     t2 = mytimer();
 
     temps = t2 - t1;
-    printf("Final number of living cells = %d\n", num_alive);
-    printf("time=%.2lf ms\n",(double)temps * 1.e3);
-	*/
-    #ifdef OUTPUT_BOARD
-    output_board( BS, &(cell(1, 1)), ldboard, maxloop);
-    #endif
+    MPI_Allreduce(&temps, &real_time, 1,MPI_DOUBLE, MPI_MAX, MPI_COMM_WORLD);
+    MPI_Allreduce(&num_alive, &num_alive, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+    if(myrank == 0)
+    {
+    	printf("Final number of living cells = %d\n", num_alive);
+    	printf("time=%.2lf ms\n",(double)temps * 1.e3);
+	}
     free(counts);
     free(displs);
     free(board);
